cb14cc5f6b21bb8c6c32b18c5665824eea4b2621
Guihii <guihiirodrigues@gmail.com>
2022-08-18 16:48:46 -0300

    Fatorial

---
+++ Laços/src/fatorial.java
@@ -0,0 +1,13 @@
+public class fatorial {
+    
+
+    public static void main(String[] args) {
+        
+        int fatorial = 1;
+        for(int n = 1; n <= 10; n++){
+            fatorial = fatorial * n;
+
+            System.out.println(fatorial);
+        }
+    }
+}
---
+++ Laços/src/lacos.java
@@ -0,0 +1,14 @@
+public class lacos {
+
+    public static void main(String[] args) {
+        
+        /* Imprima todos os multiplos de 3, entre 1 e 100*/
+
+        for(int i = 0; i < 100; i++){{
+
+        }
+            
+        }
+    }
+    
+}
