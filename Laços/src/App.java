0385b15eaeb34f43b5a2247b0cfa481dd0e46451
Guihii <guihiirodrigues@gmail.com>
2022-08-18 15:56:33 -0300

    Exercicio 1

---
+++ Laços/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        
+        /* Imprima todos os números de 150 a 300 */
+
+        int i = 150;
+
+        while (i<300){
+            System.out.println(i);
+
+            i = i+1;
+
+            
+        }
+
+
+    }
+}
